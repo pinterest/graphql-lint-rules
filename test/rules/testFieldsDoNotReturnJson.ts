b354d772e14358c218f53072eef424c8d8471b72
Maurício Gardini <mauriciogardini@users.noreply.github.com>
2022-07-15 15:06:35 -0300

    feat(rule): Add rule fieldsDoNotReturnJson (#38)
    * feat(rule): Add rule fieldsDoNotReturnJson
    
    * feat(rule): Fix fieldsDoNotReturnJson file path
    
    * feat(rule): Change error message
    
    * feat(rule): Change error message

---
+++ test/rules/testFieldsDoNotReturnJson.ts
@@ -0,0 +1,158 @@
+import { FieldsDoNotReturnJson } from '../../src/rules/fieldsDoNotReturnJson';
+
+import { gql } from '../utils';
+
+import { expectPassesRule, expectFailsRule } from '../assertions';
+
+describe('FieldsDoNotReturnJson rule', () => {
+  it('allows non-JSON fields', () => {
+    expectPassesRule(
+      FieldsDoNotReturnJson,
+      gql`
+        scalar ID
+        interface Node {
+          id: ID!
+        }
+        type Entity implements Node {
+          id: ID!
+          entityId: String!
+        }
+      `,
+    );
+  });
+
+  it('disallows straight JSON field', () => {
+    expectFailsRule(
+      FieldsDoNotReturnJson,
+      gql`
+        scalar ID
+        scalar JSON
+          @specifiedBy(
+            url: "http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf"
+          )
+        interface Node {
+          id: ID!
+        }
+        type Entity implements Node {
+          id: ID!
+          entityId: String
+          other: JSON
+        }
+      `,
+      [
+        {
+          message: 'The field `Entity.other` is returning a JSON value.',
+          locations: [{ line: 13, column: 11 }],
+        },
+      ],
+    );
+  });
+
+  it('disallows non-null JSON field', () => {
+    expectFailsRule(
+      FieldsDoNotReturnJson,
+      gql`
+        scalar ID
+        scalar JSON
+          @specifiedBy(
+            url: "http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf"
+          )
+        interface Node {
+          id: ID!
+        }
+        type Entity implements Node {
+          id: ID!
+          entityId: String
+          other: JSON!
+        }
+      `,
+      [
+        {
+          message: 'The field `Entity.other` is returning a JSON value.',
+          locations: [{ line: 13, column: 11 }],
+        },
+      ],
+    );
+  });
+
+  it('disallows straight JSON list field', () => {
+    expectFailsRule(
+      FieldsDoNotReturnJson,
+      gql`
+        scalar ID
+        scalar JSON
+          @specifiedBy(
+            url: "http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf"
+          )
+        interface Node {
+          id: ID!
+        }
+        type Entity implements Node {
+          id: ID!
+          entityId: String
+          other: [JSON]
+        }
+      `,
+      [
+        {
+          message: 'The field `Entity.other` is returning a JSON value.',
+          locations: [{ line: 13, column: 11 }],
+        },
+      ],
+    );
+  });
+
+  it('disallows non-null JSON list field', () => {
+    expectFailsRule(
+      FieldsDoNotReturnJson,
+      gql`
+        scalar ID
+        scalar JSON
+          @specifiedBy(
+            url: "http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf"
+          )
+        interface Node {
+          id: ID!
+        }
+        type Entity implements Node {
+          id: ID!
+          entityId: String
+          other: [JSON!]
+        }
+      `,
+      [
+        {
+          message: 'The field `Entity.other` is returning a JSON value.',
+          locations: [{ line: 13, column: 11 }],
+        },
+      ],
+    );
+  });
+
+  it('disallows non-null JSON non-null list field', () => {
+    expectFailsRule(
+      FieldsDoNotReturnJson,
+      gql`
+        scalar ID
+        scalar JSON
+          @specifiedBy(
+            url: "http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf"
+          )
+        interface Node {
+          id: ID!
+        }
+        type Entity implements Node {
+          id: ID!
+          entityId: String
+          other: [JSON!]!
+        }
+      `,
+      [
+        {
+          message: 'The field `Entity.other` is returning a JSON value.',
+          locations: [{ line: 13, column: 11 }],
+        },
+      ],
+    );
+  });
+});
